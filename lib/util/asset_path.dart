bb4ae292a0605a7a83e973cd44acc701739f8e8c
suojae <suojae.developer@gmail.com>
2024-10-04 22:03:35 +0900

    asset 경로 추가

---
+++ lib/util/asset_path.dart
@@ -0,0 +1,17 @@
+class AssetPath {
+  static const menuLogoBlack = "assets/menu_logo_black.png";
+  static const hamburger = "assets/hamburger.png";
+  static const questionHeaderImage = "assets/question_header_image.png";
+  static const recruitHeaderImage = "assets/recruit_header_image.png";
+  static const tag = "assets/tag.png";
+  static const bird = "assets/bird.jpg";
+  static const butterfly = "assets/butterfly.jpg";
+  static const flutter = "assets/flutter.jpg";
+  static const office = "assets/office.jpg";
+  static const office_2 = "assets/office_2.jpg";
+  static const detail1 = "assets/detail_1.jpg";
+  static const detail2 = "assets/detail_2.jpg";
+  static const companyDevice = "assets/company_device.png";
+  static const companyResponsibility = "assets/company_responsibility.png";
+  static const companySustainability = "assets/company_sustainability.png";
+}
